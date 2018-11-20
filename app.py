73699751515140dd0e4a7c9a04054343ecd5ff7f
Marek Cermak <macermak@redhat.com>
2018-11-20 23:12:06 +0100

    Minimal API spec setup
    - healh and readiness checks
    - schema endpoint
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   app.py

--- app.py
+++ app.py
@@ -5,11 +5,66 @@
 
 
 from flask import Flask
+from flask import jsonify
 from flask_restplus import Api
+from flask_restplus import Resource
+
+from osiris import __about__
+
+
+ABOUT = dict()
+
+with open(__about__.__file__) as f:
+    exec(f.read(), ABOUT)
 
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app,
+          version=ABOUT['__version__'],
+          title=ABOUT['__title__'],
+          description=ABOUT['__summary__'])
+
+
+ns_check = api.namespace(name='check',
+                         description="Namespace for API health checks.")
+
+
+@ns_check.route('/liveness')
+class Liveness(Resource):
+    """Health check."""
+
+    # noinspection PyMethodMayBeStatic
+    def get(self):  # pragma: no cover
+        return jsonify({
+            'name': "Build log aggregator.",
+            'version': ABOUT['__version__'],
+            'status': 'Status OK'
+        })
+
+
+@ns_check.route('/readiness')
+class Readiness(Resource):
+    """Readiness check.
+
+    Checks Ceph storage availability.
+    """
+
+    # noinspection PyMethodMayBeStatic
+    def get(self):
+        return jsonify({
+            'name': "Build log aggregator.",
+            'version': ABOUT['__version__'],
+            'status': 'Status READY'  # TODO: check Ceph
+        })
+
+
+@api.route('/api', '/schema')
+class APISchema(Resource):
+    """Swagger specification for this API."""
+
+    # noinspection PyMethodMayBeStatic
+    def get(self):  # pragma: no cover
+        return jsonify(api.__schema__)
 
 
 if __name__ == "__main__":
