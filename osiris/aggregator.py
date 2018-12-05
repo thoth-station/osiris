347078b1f7f745da713211f0782137a0a9523d18
Marek Cermak <macermak@redhat.com>
2018-12-05 12:46:46 +0100

    [WIP] Store build log in Ceph
    - handle login status (authentication check)
    - handle OC errors
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   app.py
    modified:   osiris/aggregator.py
    modified:   osiris/apis/auth.py
    modified:   osiris/apis/build.py
    modified:   osiris/exceptions.py
    modified:   osiris/response.py

--- osiris/aggregator.py
+++ osiris/aggregator.py
@@ -7,7 +7,9 @@ import hashlib
 from thoth.storages import BuildLogsStore
 
 from osiris import DEFAULT_OC_LOG_LEVEL
+
 from osiris.exceptions import OCError
+from osiris.exceptions import OCAuthenticationError
 from osiris.utils import execute_command
 
 # TODO: logging
@@ -21,6 +23,14 @@ def curl_build_log(build_id: str, log_level: int = DEFAULT_OC_LOG_LEVEL) -> str:
 
     :raises OCError: In case of OC CLI failure.
     """
+
+    out, err, ret_code = execute_command("oc whoami")
+
+    if ret_code > 0:
+        raise OCAuthenticationError(
+            payload=err.decode('utf-8')
+        )
+
     log_command = f"oc logs {build_id} --loglevel {log_level}"
 
     out, err, ret_code = execute_command(log_command)
@@ -47,6 +57,6 @@ def store_build_log(build_doc: dict) -> str:
 def retrieve_build_log(build_id: str) -> dict:
     """Retrieve build log document from Ceph by its id."""
     # TODO: logging
-    document_id: str = hashlib.sha256(build_id).hexdigest()
+    document_id: str = hashlib.sha256(build_id.encode('utf-8')).hexdigest()
 
     return ceph_store.retrieve_document(document_id)
