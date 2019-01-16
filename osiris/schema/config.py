9310f67eeb7ecffb5b6619f22c2ce400198ba91b
Marek Cermak <macermak@redhat.com>
2019-01-16 17:08:19 +0100

    [WIP] Use OpenShift and Thoth Common api
    - get rid of the need to use oc tool completely (replaced by kubernetes
    python api)
    - replace auth api by config api
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    deleted:    osiris/apis/auth.py
    deleted:    osiris/schema/auth.py
    new file:   osiris/apis/config.py
    new file:   osiris/schema/config.py
    modified:   .s2i/action_hooks/deploy
    modified:   osiris/apis/__init__.py

---
+++ osiris/schema/config.py
@@ -0,0 +1,57 @@
+# Osiris: Build log aggregator.
+
+"""Authentication API schema."""
+
+from marshmallow import fields
+from marshmallow import post_load
+from marshmallow import Schema
+
+from osiris import DEFAULT_OC_PROJECT
+
+
+class Config(object):
+
+    def __init__(self, api_key: str, token: str, context: str = None, cluster: str = None,
+                 namespace: str = None, host: str = None, port: str = None, url: str = None,
+                 username: str = None, password: str = None, verify_ssl: bool = None):
+        self.api_key: str = api_key
+        self.token: str = token
+
+        self.context: str = context
+        self.cluster: str = cluster
+        self.namespace: str = namespace
+
+        self.host: str = host
+        self.port: str = port
+        self.url: str = url
+
+        self.username: str = username
+        self.password: str = password
+
+        self.verify_ssl: bool = verify_ssl
+
+
+class ConfigSchema(Schema):
+
+    api_key = fields.Raw(required=True)
+    token = fields.String(required=True)
+
+    context = fields.String(
+        required=False)  # if not provided, use localhost
+    cluster = fields.String(
+        required=False)  # if not provided, use localhost
+    namespace = fields.String(
+        required=False, default=DEFAULT_OC_PROJECT, allow_none=True)
+
+    host = fields.String(allow_none=True)
+    port = fields.String(allow_none=True)
+    url = fields.Url(allow_none=True)
+
+    username = fields.String(allow_none=True)
+    password = fields.String(allow_none=True)
+
+    verify_ssl = fields.Bool(default=False)
+
+    @post_load
+    def make_user(self, data: dict) -> Config:
+        return Config(**data)
