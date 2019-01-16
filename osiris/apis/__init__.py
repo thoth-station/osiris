0f8a8cccbdea5ac9ed2cbbbd5081e3504a05393c
Marek Cermak <macermak@redhat.com>
2019-01-16 17:08:19 +0100

    [WIP] Use OpenShift and Thoth Common api
    - get rid of the need to use oc tool completely (replaced by kubernetes
    python api)
    - replace auth api by config api
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    deleted:    osiris/apis/auth.py
    deleted:    osiris/schema/auth.py
    new file:   osiris/apis/config.py
    new file:   osiris/schema/config.py
    modified:   .s2i/action_hooks/deploy
    modified:   osiris/apis/__init__.py

--- osiris/apis/__init__.py
+++ osiris/apis/__init__.py
@@ -6,7 +6,7 @@ from osiris import ABOUT
 
 from .build import api as build_namespace
 from .probes import api as probes_namespace
-from .auth import api as auth_namespace
+from .config import api as config_namespace
 
 from .model import app_data
 from .model import response
@@ -23,7 +23,7 @@ api = Api(
 
 api.add_namespace(build_namespace)
 api.add_namespace(probes_namespace)
-api.add_namespace(auth_namespace)
+api.add_namespace(config_namespace)
 
 api.add_model('status', status)
 api.add_model('app_data', app_data)
