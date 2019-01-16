efab0471c5fa6043fb5b1ebe07c0518387d53bd2
Marek Cermak <macermak@redhat.com>
2019-01-16 17:08:19 +0100

    [WIP] Use OpenShift and Thoth Common api
    - get rid of the need to use oc tool completely (replaced by kubernetes
    python api)
    - replace auth api by config api
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    deleted:    osiris/apis/auth.py
    deleted:    osiris/schema/auth.py
    new file:   osiris/apis/config.py
    new file:   osiris/schema/config.py
    modified:   .s2i/action_hooks/deploy
    modified:   osiris/apis/__init__.py

---
+++ osiris/apis/config.py
@@ -0,0 +1,119 @@
+# Osiris: Build log aggregator.
+
+"""Namespace: auth"""
+
+from http import HTTPStatus
+from typing import Union
+
+from flask_restplus import fields
+from flask_restplus import Namespace
+from flask_restplus import Resource
+
+from marshmallow import ValidationError
+
+from openshift.config import list_kube_config_contexts
+
+from osiris import get_oc_client
+
+from osiris.apis.model import response
+from osiris.schema.config import ConfigSchema
+
+from osiris.response import request_ok
+
+from osiris.exceptions import OCError
+
+from werkzeug.exceptions import HTTPException, InternalServerError
+
+
+api = Namespace(name='config',
+                description="Namespace for API configuration.",
+                validate=True)
+
+
+@api.errorhandler(ValidationError)
+def propagate_validation_error(
+        error: Union[OCError, ValidationError]):
+    """Propagate login error to the global app error handler."""
+    raise error  # re-raise
+
+
+@api.errorhandler(HTTPException)
+@api.errorhandler(InternalServerError)
+def propagate_internal_server_error(
+        error: Union[HTTPException, InternalServerError]):
+    """Propagate internal server error to the global app error handler."""
+    raise error  # re-raise
+
+
+@api.errorhandler(Exception)
+def propagate_unknown_exception(error: Exception):
+    """Propagate unknown exception to the global app error handler."""
+    raise error  # re-raise
+
+
+config_fields = api.model('config_fields', {
+    'api_key': fields.Raw(
+        required=True,
+        description="API authentication header.",
+        example={"authorization": "<token>"}
+    ),
+    'context': fields.String(
+        required=True,
+        description="Current context",
+        example="<namespace>/<cluster>/<username>"
+    ),
+    'host': fields.String(
+        required=False,
+        description="Host the user is currently logged into.",
+        example="<host>:<port>"
+    ),
+    'namespace': fields.String(
+        required=False,
+        description="Current Namespace.",
+        example="<namespace>"
+    ),
+    'verify_ssl': fields.Boolean(
+        required=False,
+        description="Whether to verify SSL",
+        example=False
+    ),
+})
+
+config_response = api.inherit('config_response', response, {
+    'payload': fields.Nested(config_fields)
+})
+
+
+@api.route("/config")
+class ConfigResource(Resource):
+    """Authentication endpoint."""
+
+    @api.marshal_with(config_response)
+    @api.doc(responses={
+        s.value: s.description for s in [
+            HTTPStatus.OK,
+        ]})
+    def get(self):
+        """Get current client configuration."""
+        schema = ConfigSchema()
+
+        client = get_oc_client()
+        client_config = client.ocp_client.configuration
+        client_config.token = client.token
+
+        context = None
+        errors = dict()
+        try:
+            _, context = list_kube_config_contexts()
+        except (FileNotFoundError, KeyError) as exc:
+            errors['context'] = str(exc)
+
+        client_config.context = context['name']
+        client_config.cluster = context['context']['cluster']
+        client_config.namespace = context['context']['namespace']
+        client_config.username, _ = context['context']['user'].split('/')
+
+        return request_ok(
+            payload=schema.dump(client_config.__dict__),
+            errors=errors
+        )
